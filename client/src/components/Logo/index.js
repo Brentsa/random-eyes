02e011422b8dcc63ff36a099727bbc1a66a7333e
Swan Pham <accounting@nailsrus.ca>
2021-07-24 19:27:46 -0400

    add background

--- client/src/components/Logo/index.js
+++ client/src/components/Logo/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import eyes from '../../assets/images/AdobeStock_300838298 (1).jpg@1.jpg'
+// import eyes from '../../assets/images/AdobeStock_300838298 (1).jpg@1.jpg'
 
 
 const Logo = () => {
   return (
     <div className='logo-wrapper'>
-      <a href="/"><img src={eyes} alt="test"/></a>
+      {/* <a href="/"><img src={eyes} alt="test"/></a> */}
+      <a href="/">Randomeyes</a>
+      
+      
     </div>
   );
 };
