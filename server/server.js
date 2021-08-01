3de32a3accf9e6468d2bbe170b3e1d6ee70fbd8f
Solidshots <seanbrent5@gmail.com>
2021-08-01 17:00:08 -0400

    bug fix

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ async function startApolloServer(typeDefs, resolvers, context)
 
     if (process.env.NODE_ENV === 'production') {
         app.use(express.static(path.join(__dirname, '../client/build')));
-        app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../client/build/index.html')));
+        //app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../client/build/index.html')));
     }
 
     server.applyMiddleware({app});
