da4e5f2ba3559df332d5dd49182ad713bdfc46a3
Swan Pham <accounting@nailsrus.ca>
2021-08-01 01:15:24 -0400

    add footer info

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="footer-background">
       
-      <h2><span>&copy;{year} Team 2</span></h2>
+      <h2><span>&copy;{year} Sean, Peter, Danyal, Swan </span></h2>
     </footer>
   );
 }
