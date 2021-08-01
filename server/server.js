fa5b43a481828d43a890488eb6b8fc1b40d09bbf
Solidshots <seanbrent5@gmail.com>
2021-08-01 19:25:16 -0400

    working version

--- server/server.js
+++ server/server.js
@@ -20,16 +20,12 @@ async function startApolloServer(typeDefs, resolvers, context)
 
     if (process.env.NODE_ENV === 'production') {
         app.use(express.static(path.join(__dirname, '../client/build')));
-
-        app.get("/service-worker.js", (req, res) => {
-            res.sendFile(path.resolve(__dirname, "../client/build/service-worker.js"));
-        });
     }
 
     server.applyMiddleware({app});
 
     app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
     });
 
     await new Promise(() => { 
