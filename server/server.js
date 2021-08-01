15e1fa307c63b571a50679a8d7a03c9aeb4abb92
Solidshots <seanbrent5@gmail.com>
2021-08-01 18:29:10 -0400

    heroku Troubleshooting

--- server/server.js
+++ server/server.js
@@ -55,6 +55,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
