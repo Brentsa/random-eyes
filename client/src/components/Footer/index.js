4e1d9f83dbd9fd96902c36bafd6d414efea503b4
Solidshots <seanbrent5@gmail.com>
2021-08-30 10:21:14 -0400

    slight style changes

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="footer-background">
       
-      <h2><span>&copy;{year} Sean, Peter, Danyal, Swan </span></h2>
+      <h2><span>&copy;{year} - Randomeyes - find something fun! </span></h2>
     </footer>
   );
 }
